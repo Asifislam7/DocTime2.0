48989d80a5274683f79aed310a109f5221b6764e
Asif <asifislam10168@gmail.com>
2026-02-10 12:43:11 +0530

    remove mapped files

--- backend/dist/middleware/errorHandler.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare class ApiError extends Error {
-    statusCode: number;
-    isOperational: boolean;
-    constructor(message: string, statusCode: number, isOperational?: boolean);
-}
-export declare const errorHandler: (err: Error | ApiError, req: Request, res: Response, next: NextFunction) => void;
-//# sourceMappingURL=errorHandler.d.ts.map
--- backend/dist/models/Prescription.model.d.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-import { IPrescription } from '../types/prescription.types';
-declare const Prescription: mongoose.Model<IPrescription, {}, {}, {}, mongoose.Document<unknown, {}, IPrescription, {}, {}> & IPrescription & Required<{
-    _id: string;
-}> & {
-    __v: number;
-}, any>;
-export { Prescription };
-//# sourceMappingURL=Prescription.model.d.ts.map
